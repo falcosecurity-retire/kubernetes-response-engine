21b4250352e31e5e45f5c0bc8f9a13423ab5e73f
Néstor Salceda <nestor.salceda@gmail.com>
2019-04-05 13:05:56 +0200

    Fix build issues

--- falco-pubsub/main.go
+++ falco-pubsub/main.go
@@ -1,3 +1,18 @@
+// Copyright 2012-2019 The Sysdig Tech Marketing Team
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build ignore
+
 package main
 
 import (
@@ -94,8 +109,6 @@ func publish(pubsubClient *pubsub.Client, msg []byte, topicName string, wg *sync
 	defer wg.Done()
 
 	alert := parseAlert(msg)
-
-	// googleTopicName := subjectAndRuleSlug(alert)
 
 	topic := pubsubClient.Topic(topicName)
 	defer topic.Stop()
