5b30d9731c0e6b325e47787eed3a85f47f7d2657
Néstor Salceda <nestor.salceda@gmail.com>
2019-04-05 17:32:49 +0200

    On AWS we relay on environment variables instead of configuring the provider

--- deployment/google-cloud/pubsub.tf
+++ deployment/google-cloud/pubsub.tf
@@ -1,18 +1,3 @@
-variable "gcloud_project" {
-}
-
-variable "gcloud_zone" {
-}
-
-variable "gcloud_credentials" {
-}
-
-provider "google" {
-  credentials = "${var.gcloud_credentials}"
-  project = "${var.gcloud_project}"
-  zone = "${var.gcloud_zone}"
-}
-
 resource "google_pubsub_topic" "falco_alerts" {
   name = "falco-alerts"
 }
--- deployment/google-cloud/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-gcloud_credentials = ""
-gcloud_project = "" 
-gcloud_zone = ""
